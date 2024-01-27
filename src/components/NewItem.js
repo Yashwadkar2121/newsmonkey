0255f1bdd9def2eba0a3fe264670660047e93e52
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-01-28 03:12:26 +0530

    Add Previous and Next Button

---
+++ src/components/NewItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class NewItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://images.moneycontrol.com/static-mcnews/2024/01/Stock-Markets-770x433.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More..
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/News.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import NewsItem from "./NewItem";
+
+export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=42f88537dabc4894b27251c31befdc3e&page=1&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handalPreClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=42f88537dabc4894b27251c31befdc3e&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles, page: this.state.page - 1 });
+  };
+
+  handalNextClick = async () => {
+    console.log("next");
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=42f88537dabc4894b27251c31befdc3e&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container my-3 ">
+          <h1 className="text-center">NewMonkey - Top Headlines</h1>
+          <div className="row my-4">
+            {this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handalPreClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handalNextClick}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </>
+    );
+  }
+}
